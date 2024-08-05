b6a1a572f0054e211b864a842c520a9a3c906a58
MasterCodeMFE <manufern@c1r1s5.42madrid.com>
2024-08-05 15:10:56 +0200

    arreglos menores

--- src/redirection/redirections.c
+++ src/redirection/redirections.c
@@ -6,7 +6,7 @@
 /*   By: manufern <manufern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 13:08:39 by cfeliz-r          #+#    #+#             */
-/*   Updated: 2024/08/05 14:14:54 by manufern         ###   ########.fr       */
+/*   Updated: 2024/08/05 15:10:15 by manufern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,79 +14,88 @@
 
 void handle_redirections(t_command *command)
 {
-    int fd;
-    
-    if (command->input_redirection)
-    {
-
-        fd = open(command->input_redirection, O_RDONLY);
-        if (fd == -1)
-           perror("open input_redirection");
-        
-        if (dup2(fd, STDIN_FILENO) == -1)
-            perror("dup2 input_redirection");
-        close(fd);
-    }
-    if (command->output_redirection)
-    {
-        int fd2;
-        if (command->append_output)
-            fd2 = open(command->output_redirection, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        else
-            fd2 = open(command->output_redirection, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-        if (fd2 == -1)
-            perror("open output_redirection");
-        if (dup2(fd2, STDOUT_FILENO) == -1)
-            perror("dup2 output_redirection");
-        close(fd2);
-    }
+	int fd;
+	int fd2;
+	
+	if (command->input_redirection)
+	{
+		fd = open(command->input_redirection, O_RDONLY);
+		if (fd == -1)
+		perror("open input_redirection");
+		
+		if (dup2(fd, STDIN_FILENO) == -1)
+			perror("dup2 input_redirection");
+		close(fd);
+	}
+	if (command->output_redirection)
+	{
+		if (command->append_output)
+			fd2 = open(command->output_redirection, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		else
+			fd2 = open(command->output_redirection, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd2 == -1)
+			perror("open output_redirection");
+		if (dup2(fd2, STDOUT_FILENO) == -1)
+			perror("dup2 output_redirection");
+		close(fd2);
+	}
 }
-char *ft_redir_cmd(char *input, t_list_env *envp)
+
+static int validate_command_cmd(char *command, t_list_env *envp, char *input_cmd)
 {
-    char **aux;
+    char *command_path = find_command_path(command, envp);
+    if (!command_path)
+        return (-1);
+    if (access(command_path, X_OK | F_OK) == -1)
+    {
+        ft_putstr_fd("minishell: ", 2);
+        ft_putstr_fd(input_cmd, 2);
+        ft_putstr_fd(": No such file or directory\n", 2);
+        free(command_path);
+        return (-1);
+    }
+    return 0;
+}
+
+char *process_redirection(char **aux, t_list_env *envp)
+{
     char *swap;
     char *result;
-    char *command_path;
     int i;
-    
-    char *opt_cmd = ft_strdup(input);
-    if (!opt_cmd)
-        return (NULL);
-    
-    if (ft_strncmp(opt_cmd, "<", 1) == 0)
-    {
-        aux = ft_split(opt_cmd, ' ');
-        command_path = find_command_path(aux[2], envp);
-        if(!command_path)
-            return NULL;
-        
-        if (access(command_path, X_OK | F_OK) == -1)
-        {
-            printf("minishell: %s: No such file or directory\n", aux[2]);
-            ft_putstr_fd("minishell: ", 2);
-            ft_putstr_fd(aux[1], 2);
-            ft_putstr_fd(": No such file or directory\n", 2);
-            free(opt_cmd);
-            free(command_path);
-            return (NULL);
-        }
-        
-        swap = ft_strdup(aux[1]);
-        result = ft_strdup(aux[2]);
-        result = ft_strjoin(result, " < ");
-        result = ft_strjoin(result, swap);
-        free(swap);   
-        i = 3;
-        while (aux[i])
-        {
-            result = ft_strjoin(result, " ");
-            result = ft_strjoin(result, aux[i]);
-            i++;
-        }       
-        free(opt_cmd);
+
+    if (validate_command_cmd(aux[2], envp, aux[1]) == -1)
+        return NULL;
+    swap = ft_strdup(aux[1]);
+    result = ft_strdup(aux[2]);
+    result = ft_strjoin(result, " < ");
+    result = ft_strjoin(result, swap);
+    free(swap);
+    i = 2;
+    while (aux[++i])
+	{
+        result = ft_strjoin(result, " ");
+        result = ft_strjoin(result, aux[i]);
     }
-    else
-        result = ft_strdup(opt_cmd);
-    return (result);
+    return result;
+}
+
+char *ft_redir_cmd(char *input, t_list_env *envp)
+{
+	char **aux;
+	char *result;
+	char *opt_cmd;
+	
+	opt_cmd = ft_strdup(input);
+	if (!opt_cmd)
+		return (NULL);
+	if (ft_strncmp(opt_cmd, "<", 1) == 0)
+	{
+		aux = ft_split(opt_cmd, ' ');
+		result = process_redirection(aux, envp);
+		if (!result)
+			free(aux);
+	}
+	else
+		result = ft_strdup(opt_cmd);
+	return (result);
 }
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: manufern <manufern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:14:45 by cfeliz-r          #+#    #+#             */
-/*   Updated: 2024/08/04 10:00:03 by manuel           ###   ########.fr       */
+/*   Updated: 2024/08/05 14:38:18 by manufern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,44 @@
 
 void clean_up_aux(t_command **commands)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    j = 0;
-    while (commands[j] != NULL) {
-        while (commands[j]->args[i] != NULL)
-            free(commands[j]->args[i++]);
-        free(commands[j]->args);
-        free(commands[j]->path);  // Liberar la memoria asignada para el path
-        j++;
-        i = 0;
-    }
+	i = 0;
+	j = 0;
+	while (commands[j] != NULL)
+	{
+		while (commands[j]->args[i] != NULL)
+			free(commands[j]->args[i++]);
+		free(commands[j]->args);
+		free(commands[j]->path);
+		j++;
+		i = 0;
+	}
 }
 
 void clean_up(char **args, t_command *commands, int num_cmds)
 {
-    int i;
+	int i;
 
-    if (args) {
-        for (i = 0; args[i] != NULL; ++i) {
-            free(args[i]);
-        }
-        free(args);
-    }
 	i = 0;
-    if (commands) {
-        while( i < num_cmds) 
+	if (args)
+	{
+		while (args[i] != NULL)
+			free(args[i ++]);
+		free(args);
+	}
+	i = 0;
+	if (commands)
+	{
+		while( i < num_cmds) 
 		{
-            free(commands[i].args);
-            free(commands[i].path);
+			free(commands[i].args);
+			free(commands[i].path);
 			i++;
-        }
-        free(commands);
-    }
+		}
+		free(commands);
+	}
 }
 
 int count_char(char *str, char c)
@@ -82,17 +85,19 @@ void close_pipes(t_command *commands, int num_cmds)
 
 void free_command(t_command *command)
 {
-    if (command->args)
-    {
-        int i = 0;
-        while (command->args[i])
-        {
-            free(command->args[i]);
-            i++;
-        }
-        free(command->args);
-    }
-    if (command->path) {
-        free(command->path);
-    }
+	int i;
+
+	if (command->args)
+	{
+		i = 0;
+		while (command->args[i])
+		{
+			free(command->args[i]);
+			i++;
+		}
+		free(command->args);
+	}
+	if (command->path) {
+		free(command->path);
+	}
 }

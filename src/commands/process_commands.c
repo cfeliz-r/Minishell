fe1dbc4746b09c3d6e652eae1fc6b6f7e91da8ad
CharlesFX <cfeliz-r@student.42madrid.com>
2024-08-09 12:54:19 +0200

    ajustes en la reedireccion

--- src/commands/process_commands.c
+++ src/commands/process_commands.c
@@ -6,7 +6,7 @@
 /*   By: cfeliz-r <cfeliz-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 12:43:52 by cfeliz-r          #+#    #+#             */
-/*   Updated: 2024/08/09 11:50:10 by cfeliz-r         ###   ########.fr       */
+/*   Updated: 2024/08/09 12:53:52 by cfeliz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static void child_process(t_command *commands, int i, int num_cmds, char **env_a
         exit(0);
     status = handle_redirections(&commands[i]);
     if (status == -1)
-        return ;
+        exit(0);
     if (!(ft_strncmp(commands[i].cmd_complete, "echo ", 5) == 0 || ft_strcmp(commands[i].cmd_complete, "echo") == 0 || ft_strncmp(commands[i].cmd_complete, "env ", 4) == 0 || ft_strcmp(commands[i].cmd_complete, "env")== 0 || ft_strncmp(commands[i].cmd_complete, "pwd ", 4) == 0 || ft_strcmp(commands[i].cmd_complete, "pwd" ) == 0 || ft_strncmp(commands[i].cmd_complete, "export ", 7) == 0 || ft_strcmp(commands[i].cmd_complete, "export") == 0))
     {
         if (execve(commands[i].path, commands[i].args, env_array) == -1)
@@ -63,16 +63,13 @@ void prepare_commands(t_command *commands, int num_cmds, t_list_env *envp)
         if (setup_pipes_and_fork(commands[i].pipefd, &commands[i].pid, i, num_cmds) == -1)
             return ;
         if (commands[i].pid == 0)
-        {
             child_process(commands, i, num_cmds, env_array, envp);
-            exit(0);
-        }
             
     }
-    close_pipes(commands, num_cmds);
     sa_int.sa_handler = sigint_handler_2;
     sa_int.sa_flags = 0;
     sigaction(SIGINT, &sa_int, NULL);
+    close_pipes(commands, num_cmds);
     i = -1;
     while (++i < num_cmds)
        waitpid(commands[i].pid, NULL, 0);

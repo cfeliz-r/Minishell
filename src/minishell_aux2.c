dcdc053c7eb4f570d52ac8b9c02a34a075f4c814
MasterCodeMFE <manueljudichini@gmail.com>
2024-08-26 19:22:47 +0200

    hola

--- src/minishell_aux2.c
+++ src/minishell_aux2.c
@@ -3,40 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_aux2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfeliz-r <cfeliz-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manufern <manufern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:03:15 by manufern          #+#    #+#             */
-/*   Updated: 2024/08/26 18:22:55 by cfeliz-r         ###   ########.fr       */
+/*   Updated: 2024/08/26 19:22:34 by manufern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char *ft_put_spaces(char *str)
-{
+char *ft_put_spaces(char *str) {
     int i = 0;
     int j = 0;
-    char *new_str;
-    new_str = malloc(sizeof(char) * (ft_strlen(str) * 2 + 1));
-    if (!new_str)
+    int in_single_quote = 0;
+    int in_double_quote = 0;
+    size_t str_len = ft_strlen(str);
+    char *new_str = malloc(sizeof(char) * (str_len * 2 + 1));
+    if (new_str == NULL) {
         return NULL;
-    while (str[i] != '\0')
-    {
-        if ((str[i] == '|' || str[i] == '<' || str[i] == '>') && (str[i + 1] != '\0' && str[i + 1] == str[i]))
-        {
-            new_str[j++] = ' ';
-            new_str[j++] = str[i++];
-            new_str[j++] = str[i++];
-            new_str[j++] = ' ';
-        }
-        else if (str[i] == '|' || str[i] == '<' || str[i] == '>')
-        {
-            new_str[j++] = ' ';
-            new_str[j++] = str[i++];
-            new_str[j++] = ' ';
-        }
-        else
-            new_str[j++] = str[i++];
     }
-    return (new_str[j] = '\0', new_str);
+
+    while (str[i] != '\0') {
+        // Toggle quote status
+        if (str[i] == '\'' && !in_double_quote) {
+            in_single_quote = !in_single_quote;
+            new_str[j++] = str[i++];
+        } else if (str[i] == '"' && !in_single_quote) {
+            in_double_quote = !in_double_quote;
+            new_str[j++] = str[i++];
+        } else if (str[i + 1] != '\0' && str[i + 1] == str[i] && !in_single_quote && !in_double_quote) {
+            // Add space between duplicated characters outside quotes
+            new_str[j++] = str[i++];
+            new_str[j++] = ' '; // Add space
+            new_str[j++] = str[i++];
+        } else if ((str[i] == '|' || str[i] == '<' || str[i] == '>') && !in_single_quote && !in_double_quote) {
+            // Add space around special characters outside quotes
+            new_str[j++] = ' '; // Add space before special character
+            new_str[j++] = str[i++];
+            new_str[j++] = ' '; // Add space after special character
+        } else {
+            new_str[j++] = str[i++];
+        }
+    }
+    
+    new_str[j] = '\0'; // Null-terminate the new string
+
+    return new_str;
 }

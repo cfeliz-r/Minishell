009ddbea179f0cfba8f0ca0764795674ee9eb87c
MasterCodeMFE <manueljudichini@gmail.com>
2024-09-03 16:39:35 +0200

    ft_export

--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -6,13 +6,13 @@
 /*   By: manufern <manufern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:13:44 by manufern          #+#    #+#             */
-/*   Updated: 2024/08/30 12:43:19 by manufern         ###   ########.fr       */
+/*   Updated: 2024/09/03 16:38:30 by manufern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-static void	skip_whitespace(const char **ptr)
+/* static void	skip_whitespace(const char **ptr)
 {
 	while (is_space((unsigned char)**ptr))
 		(*ptr)++;
@@ -79,6 +79,50 @@ static void	process_variable(const char **ptr, t_list_env **envp)
 		}
 		process_variable_end(var_start, var_end, envp);
 	}
+} */
+
+int compare_until_equal_sign(const char *str, const char *target)
+{
+    size_t i = 0;
+
+    while (str[i] != '=' && str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            i++;
+            continue ;
+        }
+        if (str[i] != target[i])
+            return (0);
+        i++;
+    }
+    if (str[i] == '=')
+        return (1);
+    return (0);
+}
+
+void add_export(const char *input, t_list_env **envp)
+{
+    char *aux;
+    int i;
+
+    i = 0;
+    while (is_space((unsigned char)input[i]))
+        i++;
+    aux = ft_strdup(&input[i]);
+
+    while (*envp != NULL)
+    {
+        if (compare_until_equal_sign((const char *)(*envp)->envp_content, (const char *)aux) == 1)
+        {
+            free((*envp)->envp_content);
+            (*envp)->envp_content = ft_strdup(aux);
+            free(aux);
+            return ;
+        }
+        envp = &(*envp)->next;
+    }
+    ft_lstadd_back(envp, ft_lstnew(aux));
 }
 
 void	ft_export(char *input, t_list_env **envp)
@@ -87,12 +131,13 @@ void	ft_export(char *input, t_list_env **envp)
 
 	if (!input || !envp)
 		return ;
-	if (strcmp(input, "export") == 0 && (input[6] == '\0'
+	if (ft_strcmp(input, "export") == 0 && (input[6] == '\0'
 			|| is_space((unsigned char)input[6])))
 		handle_export_no_args(envp);
-	else if (strncmp(input, "export ", 7) == 0)
+	else if (ft_strncmp(input, "export ", 7) == 0)
 	{
 		ptr = input + 7;
-		process_variable(&ptr, envp);
+ 		add_export(ptr, envp);
+		/* process_variable(&ptr, envp); */
 	}
 }

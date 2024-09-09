ae99cd524a278601c440358103a7020408356f51
MasterCodeMFE <manueljudichini@gmail.com>
2024-09-09 12:32:41 +0200

    chore: Refactor code to remove unnecessary spaces and variables

--- src/minishell_aux2.c
+++ src/minishell_aux2.c
@@ -6,7 +6,7 @@
 /*   By: manufern <manufern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:03:15 by manufern          #+#    #+#             */
-/*   Updated: 2024/09/05 18:29:50 by manufern         ###   ########.fr       */
+/*   Updated: 2024/09/09 12:26:49 by manufern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char	*ft_put_spaces(char *str)
 	new_str = malloc(sizeof(char) * (str_len * 2 + 2));
 	if (new_str == NULL)
 		return (NULL);
+
 	while (str[i] != '\0')
 	{
 		if (str[i] == '\'' && !in_double_quote)
@@ -51,13 +52,13 @@ char	*ft_put_spaces(char *str)
 			|| (str[i] == '<' && str[i + 1] == '>')
 			|| (str[i] == '>' && str[i + 1] == '<')))
 		{
-			if (str[i] != ' ' && !last_char_was_space)
+			if (i > 0 && str[i - 1] != ' ' && !last_char_was_space)
 				new_str[j++] = ' ';
 			new_str[j++] = str[i++];
 			new_str[j++] = str[i++];
-			if (str[i] != ' ' && !last_char_was_space)
+			if (str[i] != ' ' && str[i] != '\0')
 				new_str[j++] = ' ';
-			last_char_was_space = 0;
+			last_char_was_space = 1;
 		}
 		else if ((str[i] == '|' || str[i] == '<' || str[i] == '>')
 			&& !in_single_quote && !in_double_quote)
@@ -65,9 +66,9 @@ char	*ft_put_spaces(char *str)
 			if (str[i] != ' ' && !last_char_was_space)
 				new_str[j++] = ' ';
 			new_str[j++] = str[i++];
-			if (str[i] != ' ' && !last_char_was_space)
+			if (str[i] != ' ' && str[i] != '\0')
 				new_str[j++] = ' ';
-			last_char_was_space = 0;
+			last_char_was_space = 1;
 		}
 		else if (str[i] == ' ' && !in_single_quote && !in_double_quote)
 		{
@@ -84,6 +85,8 @@ char	*ft_put_spaces(char *str)
 			last_char_was_space = 0;
 		}
 	}
+
 	new_str[j] = '\0';
+	printf("new_str: %s\n", new_str);
 	return (new_str);
 }
